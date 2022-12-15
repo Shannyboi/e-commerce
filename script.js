7cbbee7c29c556369256c852477a6b915c128cbc
Shannyboi <120674872+Shannyboi@users.noreply.github.com>
2022-12-15 18:19:01 +0000

    second commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("button").addEventListener('click', clicked);
+
+function clicked() {
+    document.getElementById('para').style ="color:red;"
+    alert ("this is an alert! I repeat!")
+}
